9fe4e0086e245e8b15ce4ed50cc18f1c45a4b6fd
Rama Shinde <rama@velociters.com>
2021-07-11 18:22:17 +0530

    file added by rama

---
+++ com/velociter/rama/chapter1/Hello.java
@@ -0,0 +1,10 @@
+package com.velociter.rama.chapter1;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       System.out.println("Hi Hello, Thank you ");
+	}
+
+}
