525d8b3a6a407468fa28931f9853cda7d23206f2
grvkush96 <gourav.velociter@gmail.com>
2021-07-12 11:03:07 +0530

    changes done by gourav

--- src/com/velociter/rama/chapter1/Hello.java
+++ src/com/velociter/rama/chapter1/Hello.java
@@ -1,7 +1,7 @@
 package com.velociter.rama.chapter1;
 
 public class Hello {
-
+    int a=10;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
        System.out.println("Hi Hello, Thank you ");
