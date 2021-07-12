462adf589aba896e05a80e67b28c7aa7b94933cb
grvkush96 <gourav.velociter@gmail.com>
2021-07-12 11:47:22 +0530

    calculation done by gourav

--- src/com/velociter/rama/chapter1/Hello.java
+++ src/com/velociter/rama/chapter1/Hello.java
@@ -1,11 +1,19 @@
 package com.velociter.rama.chapter1;
 
 public class Hello {
-    int a=10;
+   static int a=10;
+   static int b=20;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
        System.out.println("Hi Hello, Thank you ");
        System.out.println("I know who is this");
+       try {
+    	   int c=a+b;
+		System.out.println(""+(a+b));
+	} catch (Exception e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
 	}
 
 }
