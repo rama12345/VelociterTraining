3606c654d43238f20ccb86bbcd76a22b7a92315e
grvkush96 <gourav.velociter@gmail.com>
2021-07-12 11:54:06 +0530

    changed by gourav

---
+++ src/com/velociter/exercise/chapter2/FirstExc1.java
@@ -0,0 +1,18 @@
+package com.velociter.exercise.chapter2;
+
+public class FirstExc1{
+	
+	public static void main(String[] args) {
+		byte a=1;
+		int i=1;
+		while(i<=8)
+		{ 
+			a=(byte)(a*2);
+
+			System.out.println(a);
+			i++;
+		
+		}
+		
+	}
+}
---
+++ src/com/velociter/exercise/chapter2/Volume.java
@@ -0,0 +1,10 @@
+package com.velociter.exercise.chapter2;
+
+public class Volume {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
