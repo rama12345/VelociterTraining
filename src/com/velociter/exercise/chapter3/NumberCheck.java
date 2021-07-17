c20ef91b9ea1bf3e933a8d20dd5ee53ec2254ed2
grvkush96 <gourav.velociter@gmail.com>
2021-07-17 14:44:41 +0530

    this programm completed and commit by gourav

---
+++ src/com/velociter/exercise/chapter3/NumberCheck.java
@@ -0,0 +1,17 @@
+package com.velociter.exercise.chapter3;
+
+public class NumberCheck {
+	public static void main(String[] args) {
+		int number=0;
+		number=1+(int)(100*Math.random());
+		if (number%2==0)
+		 { // Test if it is even
+			System.out.println("You have got an even number, " + number); // It is even
+			} else {
+			System.out.println("You have got an odd number, "+ number);
+			}// It is odd
+
+		
+	}
+
+}
